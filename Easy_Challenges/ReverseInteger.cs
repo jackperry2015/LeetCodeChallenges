250c79846a97585a89541c0f67418b465ab47c41
Jack Perry <jack.perry.2015@my.ntu.ac.uk>
2020-09-21 21:56:46 +0100

    ReverseInteger

---
+++ Easy_Challenges/ReverseInteger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace LeetCodeChallenges.Easy_Challenges
+{
+    // Reverse whatever integer is passed i.e. 123 -> 321
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            int placeholder = x;
+            int reverse = 0;
+
+            while(Convert.ToBoolean(placeholder))
+            {
+                int y = placeholder % 10;
+                reverse = reverse * 10 + y;
+                placeholder = placeholder / 10;
+            }
+            return reverse;
+        }
+    }
+}
---
+++ Tests/ReverseInteger_Tests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeChallenges.Easy_Challenges;
+using NUnit.Framework;
+
+namespace LeetCodeChallenges.Tests
+{
+    [TestFixture]
+    public class ReverseInteger_Tests
+    {
+        private ReverseInteger _reverseInteger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _reverseInteger = new ReverseInteger();
+        }
+
+        // LeetCode Test Case
+        [Test]
+        public void ReverseInteger_123Input_321Output()
+        {
+            int testCase = 123;
+            var result = _reverseInteger.Reverse(testCase);
+
+            Assert.That(result == 321);
+        }
+    }
+}
