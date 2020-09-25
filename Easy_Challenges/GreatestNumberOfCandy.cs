ba7081bb855a1e5eb35985dbe064e64ac6cdc44b
Jack Perry <jack.perry.2015@my.ntu.ac.uk>
2020-09-25 20:51:14 +0100

    GreatestNumberOfCandy Challenge

---
+++ Easy_Challenges/GreatestNumberOfCandy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCodeChallenges.Easy_Challenges
+{
+    // https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+    public class GreatestNumberOfCandy
+    {
+        public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+        {
+            int mostCandies = candies.Max();
+            List<bool> canBeGreatest = new List<bool>();
+
+            for (int i = 0; i < candies.Length; i++)
+            {
+                if (candies[i] + extraCandies >= mostCandies) canBeGreatest.Add(true);
+                else canBeGreatest.Add(false);
+            }
+            return canBeGreatest;
+        }
+    }
+}
---
+++ Tests/Easy_Challenges_Tests/GreatestNumberOfCandy_Tests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCodeChallenges.Easy_Challenges;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCodeChallenges.Tests.Easy_Challenges_Tests
+{
+    [TestFixture]
+    public class GreatestNumberOfCandy_Tests
+    {
+        private GreatestNumberOfCandy _greatestNumberOfCandy;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _greatestNumberOfCandy = new GreatestNumberOfCandy();
+        }
+
+        // LeetCode Test Case
+        [Test]
+        public void KidsWithCandies_ReturnsValid_Result()
+        {
+            int[] testArray = new int[] { 2, 3, 5, 1, 3 };
+            int testExtraCandies = 3;
+
+            var result = _greatestNumberOfCandy.KidsWithCandies(testArray, testExtraCandies);
+            var expectedResult = new List<bool> { true, true, true, false, true};
+
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
