09a9d1ba065565ba4d00c4c7870f81008cbe76f4
Jack Perry <jack.perry.2015@my.ntu.ac.uk>
2020-10-05 22:49:13 +0100

    NumberOfGoodPairs Challenge

---
+++ Easy_Challenges/NumberOfGoodPairs.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeChallenges.Easy_Challenges
+{
+    // https://leetcode.com/problems/number-of-good-pairs/
+    public class NumberOfGoodPairs
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int Count = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                for (var j = 0; j < nums.Length; j++)
+                    if (nums[i].Equals(nums[j]) && i < j)
+                        Count += 1;
+            }
+            return Count;
+        }
+    }
+}
---
+++ Tests/Easy_Challenges_Tests/NumberOfGoodPairs_Tests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeChallenges.Easy_Challenges;
+using NUnit.Framework;
+
+namespace LeetCodeChallenges.Tests.Easy_Challenges_Tests
+{
+    [TestFixture]
+    public class NumberOfGoodPairs_Tests
+    {
+        NumberOfGoodPairs _numberOfGoodPairs;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _numberOfGoodPairs = new NumberOfGoodPairs();
+        }
+
+        [TestCase(new int[] { 1,2,3,1,1,3 }, 4)]
+        [TestCase(new int[] { 1,1,1,1 }, 6)]
+        [TestCase(new int[] { 1,2,3 }, 0)]
+        public void Shuffle_Returns_CorrectShuffledArray(int[] ints, int expectedResult)
+        {
+            Assert.IsTrue(_numberOfGoodPairs.NumIdenticalPairs(ints) == expectedResult);
+        }
+    }
+}
